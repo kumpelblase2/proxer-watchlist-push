439253f8dc94a560bbb732eb65b147ee6a834445
Tim Hagemann <tim@eternalwings.de>
2016-11-22 20:35:53 +0100

    Updated to a newer phantom and phantomjs version.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var phantom = require('phantom');
-var Promise = require('bluebird');
 var helpers = require('./proxer-helpers');
 var config = require('./config');
 var PushBullet = require('pushbullet');
@@ -8,23 +7,22 @@ var schedule = require('node-schedule');
 var fs = require('fs');
 var _ = require('lodash');
 
-phantom.create('--cookies-file=cookies.txt', '--load-images=no', function (ph) {
-    ph.createPage(function(page) {
-        start(page);
-    });
+phantom.create(['--cookies-file=cookies.txt', '--load-images=no']).then(function (ph) {
+    return ph.createPage();
+}).then(function(page) {
+    start(page);
+    runJob(page);
 });
 
 
 function run(page, username, password) {
-    return helpers.login(page, username, password).then(function(result) {
-        return new Promise(function(resolve) {
-            page.open('http://proxer.me/ucp?s=reminder', resolve);
-        });
-    }).delay(1000).then(function() {
-        return helpers.waitFor(function(cb) {
-            page.evaluate(function() {
+    return helpers.login(page, username, password).then(function() {
+        return page.open('http://proxer.me/ucp?s=reminder');
+    }).then(function() {
+        return helpers.waitFor(function() {
+            return page.evaluate(function() {
                 return document.querySelector('table#box-table-a') != null;
-            }, cb);
+            });
         }, 4);
     }).then(function() {
         return helpers.extract(page);
@@ -35,6 +33,23 @@ function sendPush(item) {
     pusher.link('', 'New EP of ' + item.name + ' is up!', item.link);
 }
 
+function runJob(page) {
+    console.log('Running job');
+    var read = JSON.parse(fs.readFileSync('lastResult.json'));
+    run(page, config.username, config.password).then(function(result) {
+        console.log('Got result for ' + result.length + ' entries.');
+        result.forEach(function(item) {
+            var last = _.find(read, function(old) { return old.id == item.id; });
+            if((!last || last.status == false) && item.status == true) {
+                sendPush(item);
+            }
+        });
+
+        console.log('Finished');
+        fs.writeFileSync('lastResult.json', JSON.stringify(result, null, 4));
+    });
+}
+
 function start(page) {
     var jobs = [];
 
@@ -42,20 +57,7 @@ function start(page) {
         var rule = new schedule.RecurrenceRule();
         rule.minute = minute;
         jobs.push(schedule.scheduleJob(rule, function() {
-            console.log('Running job');
-            var read = JSON.parse(fs.readFileSync('lastResult.json'));
-            run(page, config.username, config.password).then(function(result) {
-                console.log('Got result for ' + result.length + ' entries.');
-                result.forEach(function(item) {
-                    var last = _.find(read, function(old) { return old.id == item.id; });
-                    if((!last || last.status == false) && item.status == true) {
-                        sendPush(item);
-                    }
-                });
-
-                console.log('Finished');
-                fs.writeFile('lastResult.json', JSON.stringify(result, null, 4));
-            });
+            runJob(page);
         }));
     });
 
--- proxer-helpers.js
+++ proxer-helpers.js
@@ -2,80 +2,65 @@ var Promise = require('bluebird');
 
 module.exports = {
     login: function(page, username, password) {
-        return new Promise(function(resolve) {
-            page.open('http://proxer.me', resolve);
-        }).then(function() {
-            return new Promise(function(resolve) {
-                page.evaluate(function(user, pass) {
-                    if($('#uname').length == 0) {
-                        $('input[name=username]').val(user);
-                        $('input[name=password]').val(pass);
-                        $('input[name=remember]').prop('checked', true);
-                        $('form#login-form').submit();
-                        return 'login';
-                    } else {
-                        return 'loggedIn';
-                    }
-                }, function(result) {
-                    if(result == 'loggedIn') {
-                        console.log('was already logged in.');
-                    } else {
-                        console.log('logged back in.');
-                    }
-                    setTimeout(function() {
-                        resolve(result);
-                    }, 3000);
-                }, username, password);
-            });
+        return page.open('http://proxer.me').then(function() {
+            return page.evaluate(function(user, pass) {
+                if($('#uname').length == 0) {
+                    $('input[name=username]').val(user);
+                    $('input[name=password]').val(pass);
+                    $('input[name=remember]').prop('checked', true);
+                    $('form#login-form').submit();
+                    return 'login';
+                } else {
+                    return 'loggedIn';
+                }
+            }, username, password);
+        }).then(function(result) {
+            if(result == 'loggedIn') {
+                console.log('was already logged in.');
+            } else {
+                console.log('logged back in.');
+            }
+
+            return Promise.delay(3000, result);
         });
     },
     extract: function(page) {
-        return new Promise(function(resolve) {
-            page.evaluate(function() {
-                var temp = [];
-                var tables = document.querySelectorAll('table#box-table-a');
-                for(var i = 0; i < tables.length; i++) {
-                    var content = tables[i];
-                    Array.prototype.forEach.call(content.rows, function(row) {
-                        if(/entry.+/.test(row.id)) {
-                            var title = {};
-                            title.name = row.cells[1].children[0].innerHTML;
-                            title.status = /online/.test(row.cells[5].children[0].src);
-                            title.id = /Cover:(.+)/.exec(row.cells[1].children[0].title)[1];
-                            title.link = row.cells[1].children[0].href;
-                            temp.push(title);
-                        }
-                    });
-                }
+        return page.evaluate(function() {
+            var temp = [];
+            var tables = document.querySelectorAll('table#box-table-a');
+            for(var i = 0; i < tables.length; i++) {
+                var content = tables[i];
+                Array.prototype.forEach.call(content.rows, function(row) {
+                    if(/entry.+/.test(row.id)) {
+                        var title = {};
+                        title.name = row.cells[1].children[0].innerHTML;
+                        title.status = /online/.test(row.cells[5].children[0].src);
+                        title.id = /Cover:(.+)/.exec(row.cells[1].children[0].title)[1];
+                        title.link = row.cells[1].children[0].href;
+                        temp.push(title);
+                    }
+                });
+            }
 
-                return temp;
-            }, resolve);
+            return temp;
         });
     },
     waitFor: function(cond, maxAcc) {
-        var self = this;
-        var promisify = function(resolve, reject) {
-            cond(function(result) {
-                if(result) {
-                    resolve();
-                } else {
-                    reject();
-                }
-            });
-        };
-        return new Promise(function(resolve, reject) {
-            return self._retry(promisify, maxAcc, resolve, reject);
-        });
+        return this._retry(cond, maxAcc);
     },
-    _retry: function(promise, max, resolve, reject) {
+    _retry: function(cond, max) {
+        if(max == 0) {
+            throw new Error();
+        }
+
         var self = this;
-        return new Promise(promise).then(resolve).catch(function() {
-            if(max > 0) {
-                return Promise.resolve().delay(250).then(function() {
-                    return self._retry(promise, max - 1, resolve, reject)
-                });
+        return cond().then(function(result) {
+            if(result) {
+                return true;
             } else {
-                reject();
+                return Promise.delay(250).then(function() {
+                    return self._retry(cond, max - 1);
+                });
             }
         });
     }
